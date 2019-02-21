4ed4f401bad004d4ee9d571f368029f8ecad3df1
Jason Clark <mithereal@gmail.com>
2019-02-20 21:03:28 -0700

    .

--- src/Card.js
+++ src/Card.js
@@ -14,20 +14,25 @@ let Card = (props) => {
             <div  className={"card " + props.card.type.toLowerCase() + " " + props.card.title.toLowerCase().replace(/ /g,"_") } onClick={() => props.cardClickHandeler(props.card)}>
 
                     <div className="cardint">
-                        <div className="cardfront">
-                            <div className="flipcolor"></div>
-                            <div className="cardimg">
-                                <img src={props.card.image} alt={props.card.title} />
-                            </div>
-                            <h1 className={"cardtitle" +  isActive( props.card,props.selected ) }>{props.card.title} </h1>
-                            <div className="moreinfo">
-                                Card Info »
-                            </div>
-                            <div className="cardinfolink" ></div>
-                        </div>
-                        <div className="cardback" id={ props.card.title.toLowerCase().replace(/ /g,"_") }>
-                            <h2 className="cardtype">{ props.card.type }</h2>
-                        </div>
+
+                        {(() => {
+                            switch (props.card != props.selected ) {
+                            case true:   return <div className="cardback" id={ props.card.title.toLowerCase().replace(/ /g,"_") }>
+                            <h2 className="cardtype">{ props.card.type }</h2></div>;
+                            default: return  <div className="cardfront">
+                                <div className="flipcolor"></div>
+                                <div className="cardimg">
+                                    <img src={props.card.image} alt={props.card.title} />
+                                </div>
+                                <h1 className={"cardtitle" +  isActive( props.card,props.selected ) }>{props.card.title} </h1>
+                                <div className="moreinfo">
+                                    Card Info »
+                                </div>
+                                <div className="cardinfolink" ></div>
+                            </div> ;
+                            }
+                        })()}
+
                     </div>
 
             </div>
